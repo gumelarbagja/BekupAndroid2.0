df14473340d2ab1390b3fdae536c34eade13c775
iqbalhood <iqbalhood@gmail.com>
2016-10-11 10:18:46 +0700

    input source map dan admob

---
+++ 5-googlemap-admob/GoogleMapBekup/app/src/main/java/com/bekup/googlemap/MainActivity.java
@@ -0,0 +1,40 @@
+package com.bekup.googlemap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPlain, btnMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlain    = (Button)findViewById(R.id.btnPlan);
+        btnMarker   = (Button)findViewById(R.id.btnMarker);
+
+
+        btnPlain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent k = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(k);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+    }
+}
