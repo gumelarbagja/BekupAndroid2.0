982779463abfef681d30e70a139a47b23cc22edb
Deni Rohimat <rohimatdeni@gmail.com>
2016-10-17 15:53:51 +0700

    add image loader in get object sample from API

--- 3-ListViewAdapterInteractiveWithAPI/app/src/main/java/com/bekup_portal/androidlesson3/GetObjectActivity.java
+++ 3-ListViewAdapterInteractiveWithAPI/app/src/main/java/com/bekup_portal/androidlesson3/GetObjectActivity.java
@@ -1,12 +1,15 @@
 package com.bekup_portal.androidlesson3;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bekup_portal.androidlesson3.models.User;
 import com.bekup_portal.androidlesson3.networks.GitHubService;
+import com.bumptech.glide.Glide;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,26 +28,38 @@ public class GetObjectActivity extends AppCompatActivity {
         tvUser = (TextView) findViewById(R.id.tv_username);
         tvBio = (TextView) findViewById(R.id.tv_bio);
 
+        mImageProfilePict = (ImageView) findViewById(R.id.iv_profile_pic);
+
         loadData();
     }
 
     private void loadData() {
+        final ProgressDialog progressDialog = new ProgressDialog(GetObjectActivity.this);
+        progressDialog.setMessage("Load data...");
+        progressDialog.show();
+
         GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
         Call<User> call = gitHubService.getUser();
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
+                progressDialog.show();
                 User user = response.body();
 
                 if (user != null) {
                     tvUser.setText(user.getLogin());
                     tvBio.setText(user.getBio());
+
+                    Glide.with(GetObjectActivity.this)
+                            .load(user.getAvatar_url())
+                            .into(mImageProfilePict);
                 }
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
-
+                progressDialog.show();
+                Toast.makeText(GetObjectActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
